7988bec34f3fe78fd6f5c15205ed241b3632380e
yd-tw <yd960528@gmail.com>
2023-12-23 00:17:20 +0800

    fix rogic

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,3 +1,6 @@
+//install url
+//https://software-metadata.revrobotics.com/REVLib-2023.json
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
@@ -10,26 +13,25 @@ public class Robot extends TimedRobot {
   private Joystick m_stick;
   private CANSparkMax m_motor;
   private RelativeEncoder m_encoder;
+  private double position;
+  private double speed;
 
   @Override
   public void robotInit() {
     m_motor = new CANSparkMax(4, MotorType.kBrushless);
+    m_stick = new Joystick(0);
     m_motor.restoreFactoryDefaults();
     m_encoder = m_motor.getEncoder();
-    m_stick = new Joystick(0);
-
-    m_motor.getEncoder().setPosition(0);
+    m_encoder.setPosition(0);
   }
 
   @Override
   public void teleopPeriodic() {
     m_motor.set(m_stick.getRawAxis(0) * 0.3);
 
-    double speed = Math.round(m_encoder.getVelocity() * 1.0) / 1.0;
+    position = Math.round(m_encoder.getPosition() * 1000) / 1000;
+    speed = Math.round(m_encoder.getVelocity() * 1000) / 1000;
 
-
-    System.out.print(m_stick.getRawAxis(0) + ",   ");
-    System.out.print(m_encoder.getPosition() + ",   ");
-    System.out.println(speed);
+    System.out.println("position: " + position + ", speed: " + speed);
   }
 }
